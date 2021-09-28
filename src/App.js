355a371b34ed3b5f568b1b953dcaced24d081d46
Lenka Zivkovic <lenkazivkovic@Lenkas-MacBook-Pro.local>
2021-09-28 12:00:02 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from "react";
 
 function App() {
+
+  const [state, setstate] = useState([]);
+
+  useEffect(() => {
+   fetc("https://api.imgflip.com/get_memes").then(x => x.json().then(response => ))
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
